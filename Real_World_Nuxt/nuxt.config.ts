d9f406672406c1bace1f339883686d5b1c858d9b
Simon Meier <116065827+abbsimmei@users.noreply.github.com>
2025-02-04 08:25:03 +0100

    changed config

--- Real_World_Nuxt/nuxt.config.ts
+++ Real_World_Nuxt/nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true,
+  nitro: {
+    preset: 'node-server', // Run as a Node.js server
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   css: ['~/assets/css/main.css'],
